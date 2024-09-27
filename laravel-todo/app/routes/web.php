51938ca1ce824ff0a17800f63a7d1b7b28c9f783
fox3238 <masaya.miyazaki1529@gmail.com>
2024-09-27 14:03:42 +0900

    last

--- laravel-todo/app/routes/web.php
+++ laravel-todo/app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth; // Authのuse文を追加
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ここから追加
+Route::resource('todo', 'App\Http\Controllers\TodoController');
+Route::put('todo/done/{todo}', [App\Http\Controllers\TodoController::class, 'done'])->name('todo.done');
+Route::put('todo/undone/{todo}', [App\Http\Controllers\TodoController::class, 'undone'])->name('todo.undone');
+//　ここまで追加
